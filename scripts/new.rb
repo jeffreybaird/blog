883e9d6c46ddac746d002c5d21a6a001f0655247
jeffreybaird <jbaird@sweetspotdiabetes.com>
2015-03-19 16:47:00 -0700

    adding a new post generator

---
+++ scripts/new.rb
@@ -0,0 +1,19 @@
+puts "what is the title of your new post?"
+
+title = gets.chomp
+
+template = title.gsub(" ", "_").downcase.strip + ".erb"
+
+template_string =<<EOF
+<%
+# title:: #{title}%
+# description::%
+# date::#{Time.now.strftime("%Y-%m-%d")}%
+# author:: Jeffrey Baird%
+%>
+
+<ul style="list-style:none;"><li><%= link_to("/", "Home") %></li><li><%= link_to("/blog", "Blog") %></li></ul>
+EOF
+
+File.open("views/posts/#{template.strip}", "w+") { |file| file.write(template_string)  }
+
